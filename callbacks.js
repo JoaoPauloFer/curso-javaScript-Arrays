5ac805ca657c3eb5ff99f17f88c6e86feeb492f7
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-03 12:17:45 -0300

    docs: 📑  Métodos Callback

---
+++ callbacks.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Ju", "João", "Paulo"];
+
+nomes.forEach(ImprimeNomes);
+
+function ImprimeNomes(nomes){
+    console.log(nomes)
+}
---
+++ map.js
@@ -0,0 +1,8 @@
+let nomes = ["ana Julia", "Caio Vinicius", "BIA silva"]
+
+const nomesAtualizados = nomes.map((nome )=> {
+    const maiuscula = nome.toUpperCase();
+    return maiuscula; 
+})
+
+console.log(nomesAtualizados)
